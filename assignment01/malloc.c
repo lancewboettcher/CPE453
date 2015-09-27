93f03aac5a0e510be55dda93d66edd1be4b88d38
Lance Boettcher <lboettch@calpoly.edu>
2015-09-27 15:34:10 -0700

    working on malloc helpers

--- assignment01/malloc.c
+++ assignment01/malloc.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 
 #define DEFAULT_CHUNK_SIZE 65536
+#define TRUE 1
+#define FALSE 0
 
 typedef struct blockHeader {
    size_t size;
@@ -10,7 +12,7 @@ typedef struct blockHeader {
    struct blockHeader *next;
 } blockHeader;
 
-void *head = NULL;
+blockHeader *head = NULL;
 
 int main (int argc, char *argv[], char *envp[])
 {
@@ -18,19 +20,32 @@ int main (int argc, char *argv[], char *envp[])
    return 0;
 }
 
-void *calloc(size t nmemb, size t size) {
+void *calloc(size t nmemb, size_t size) {
 #ifdef DEBUG_MALLOC
    printf("Called my calloc\n");
 #endif
+
+
    
 }
 
-void *malloc(size t size) {
+void *malloc(size_t size) {
 #ifdef DEBUG_MALLOC
    printf("Called my malloc\n");
 #endif
 
+   if (size <= 0 ) {
+      return NULL;
+   }
 
+   if (head == NULL) {
+      /* First time calling malloc */ 
+      head = allocateMemory(DEFAULT_CHUNK_SIZE, NULL);
+
+      if (head == NULL) {
+         return NULL;
+      }
+   }
 
 }
 
@@ -40,9 +55,10 @@ void free(void *ptr) {
 #endif
 
 
+
 }
 
-void *realloc(void *ptr, size t size) {
+void *realloc(void *ptr, size_t size) {
 #ifdef DEBUG_MALLOC
    printf("Called my realloc\n");
 #endif
@@ -60,13 +76,50 @@ void *realloc(void *ptr, size t size) {
  * Helpers
 */
 
-struct blockHeader getFreeBlock(size_t size, ) {
+blockHeader *getFreeBlock(size_t size, blockHeader **prev) {
 
+   blockHeader *iterator = head;
 
+   while (iterator != NULL && !iterator->isFree && iterator->size < size) {
+      /* Looking for a free block big enough */
 
+      *prev = iterator;
+      iterator = iterator->next;
+   }
+
+   if (iterator == NULL) {
+      /* Nothing big enough */
+      
+      if (size > DEFAULT_CHUNK_SIZE) {
+         iterator = allocateMemory(size, *prev);
+      }
+      else {
+         iterator = allocateMemory(DEFAULT_CHUNK_SIZE);
+      }
+   }
+
+   return iterator;
 }
 
-struct blockHeader allocateMoreMemory(size_t size, ) {
+blockHeader *allocateMemory(size_t size, blockHeader *prev) {
 
+   blockHeader *newBlock = sbrk(size + sizeof(blockHeader));
 
+   if (newBlock == (void *) -1) {
+      errno = ENOMEM;
+      return NULL;
+   }
+
+   if (prev == NULL) {
+      head = newBlock;
+   }
+   else {
+      prev->next = newBlock;
+   }
+
+   newBlock->size = size + sizeof(blockHeader);
+   newBlock->next = NULL;
+   newBlock->isFree = FALSE;
+
+   return newBlock;
 }
