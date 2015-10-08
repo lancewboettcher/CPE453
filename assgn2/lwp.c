706952a94556f2b22c25ce298a244ae1ffe9d51b
csardo <csardo@calpoly.edu>
2015-10-08 15:25:04 -0700

    added initial lwp_create implementation

--- assgn2/lwp.c
+++ assgn2/lwp.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include "lwp.h"
 
-extern tid_t lwp_create(lwpfun,void *,size_t) {
+scheduler sched = NULL;
+unsigned long uniqueId = 0;
 
+extern tid_t lwp_create(lwpfun function, void * args, size_t stackSize) {
+   void *stackPtr; 
+
+   if (!sched) {
+      sched = malloc(sizeof(scheduler));
+   }
+
+   thread *newThread = malloc(sizeof(context));
+   newThread->stack = malloc(stackSize);
+   newThread->stacksize = stackSize;
+   newThread->tid = uniqueId++;
+
+   /* Setup stack */
+   stackPtr = (void *)newThread->stack + stackSize;
+   
+   stackPtr -= 7 * sizeof(tid_t); /* arguments */
+   *stackPtr = args;
+   stackPtr -= sizeof(tid_t); /* return address */
+   *stackPtr = (tid_t)lwp_exit;
+   stackPtr -= sizeof(lwpfun); /* function to call */
+   *stackPtr = function;
+ 
+   newThread->stack.rbp = (tid_t)stackPtr;
+   newThread->stack.rsp = (tid_t)stackPtr;
+   
+   /* Setup registers? */
+
+   sched.admit(newThread);
 }
 
 extern void  lwp_exit(void) {
