cea119a9ba48337961aad15530a2fbd9fb6738d8
Lance Boettcher <lboettch@csclnx11.ad.calpoly.edu>
2015-12-04 13:23:28 -0800

    asgn6

---
+++ asgn6/hello.c
@@ -0,0 +1,17 @@
+/* 
+ * CPE 453
+ * Assignment 6 - Hello 
+ * Lance Boettcher (lboettch) 
+ *
+ * Greets the world by printing "Hello, world!”
+ *
+*/
+
+#include <stdio.h> 
+
+int main(int argc, char *argv[]) {
+
+   printf("Hello, world!\n"); 
+
+   return 0;
+}   
